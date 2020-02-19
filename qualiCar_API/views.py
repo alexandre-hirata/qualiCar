3949c73ea9ac00356600e8a80eb64d311ecb2164
Alexandre Hirata <alexthirata@gmail.com>
2020-02-19 15:25:16 -0300

    Add Serializer to APIView
    Signed-off-by: Alexandre Hirata <alexthirata@gmail.com>

--- qualiCar_API/views.py
+++ qualiCar_API/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from qualiCar_API import serializers
 
 
 class qualiCarApiView (APIView):
     """ qualiCar API View """
+    serializer_class = serializers.qualiCarSerializer
 
     def get (self, request, format=None):
         """ Returns a list of APIView feature """
@@ -17,3 +21,18 @@ class qualiCarApiView (APIView):
 
         # It converts the response to JSon
         return Response ({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post (self, request):
+        """ Create a hello message with our name """
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response ({'message': message})
+        else:
+            return Response (
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
