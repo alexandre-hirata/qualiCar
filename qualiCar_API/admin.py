03d4a0e1c39b4e6bf57185b62f854ddc8a3a00a4
Alexandre Hirata <alexthirata@gmail.com>
2020-02-29 12:32:05 -0300

    Modify admin to support changing user model
    Signed-off-by: Alexandre Hirata <alexthirata@gmail.com>

--- qualiCar_API/admin.py
+++ qualiCar_API/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from qualiCar_API import models
 
 admin.site.register (models.Date)
+
+
+class UserAdmin (BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+admin.site.register (models.UserProfile, UserAdmin)
