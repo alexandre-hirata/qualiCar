edc10c47f658fbb6dd36cc2d66dd7781da74615b
Alexandre Hirata <alexthirata@gmail.com>
2020-03-02 15:48:24 -0300

    Add test_waiting_for_db
    Signed-off-by: Alexandre Hirata <alexthirata@gmail.com>

--- qualiCar_API/tests/test_commands.py
+++ qualiCar_API/tests/test_commands.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 
 from django.core.management import call_command
+from django.db.utils import OperationalError
 from django.test import TestCase
 
 
@@ -11,7 +12,19 @@ class CommandTests (TestCase):
         # This command replaces the behavior of __getitem__ as getitem and returns True
         with patch ('django.db.utils.ConnectionHandler.__getitem__') as getitem:
             getitem.return_value = True
-            call_command (' wait_for_db')
+            call_command ('wait_for_db')
 
             # The mock also counts how many times it was called
             self.assertEqual (getitem.call_count, 1)
+
+    # This command makes the same as with patch, but before the function
+    # definition
+    @patch ('time.sleep', return_value = True)
+    def test_wait_for_db (self, ts):
+        """ Test waiting for db """
+        with patch ('django.db.utils.ConnectionHandler.__getitem__') as getitem:
+            # This command makes timeout 5 times and in sixth return True
+            getitem.side_effect = [OperationalError] * 5 + [True]
+            call_command ('wait_for_db')
+
+            self.assertEqual (getitem.call_count, 6)
