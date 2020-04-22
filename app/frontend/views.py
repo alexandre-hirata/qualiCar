3adfb5a0b5340bf4da47e8ec8fdd52ef896df0bf
Alexandre Hirata <alexthirata@gmail.com>
2020-04-22 12:35:01 -0300

    Implement working login page
    The pages does not recognize logged user yet.
    
    Signed-off-by: Alexandre Hirata <alexthirata@gmail.com>

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -3,6 +3,14 @@ from rest_framework.views import APIView
 from django.views import View
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponseRedirect
+
+from qualiCar_API.models import UserProfile
+
 
 
 # class Index (APIView):
@@ -20,4 +28,16 @@ class Login (View):
     template = 'login.html'
 
     def get (self, request):
-        return render(request, self.template)
+        form = AuthenticationForm()
+        return render(request, self.template, {'form': form})
+
+    def post (self, request):
+        form = AuthenticationForm (request.POST)
+        username = request.POST.get ('username')
+        password = request.POST.get ('password')
+        user = authenticate (request, username=username, password=password)
+        if user is not None:
+            login (request, user)
+            return HttpResponseRedirect ('/')
+        else:
+            return render (request, self.template, {'form': form})
