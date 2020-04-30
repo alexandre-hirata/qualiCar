eb88271e63cac4c6b12079eb392dc2af480b8acc
Alexandre Hirata <alexthirata@gmail.com>
2020-04-30 15:21:23 -0300

    Transfer admin page to frontend app
    Signed-off-by: Alexandre Hirata <alexthirata@gmail.com>

--- app/frontend/urls.py
+++ app/frontend/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from frontend import views
+from django.contrib import admin
+
 
 urlpatterns = [
     path('', views.Index.as_view (), name='index'),
     path('login', views.Login.as_view(), name='login'),
+    path('admin', admin.site.urls, name='admin'),
 ]
