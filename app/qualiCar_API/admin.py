95b81f07344e8338b4a7e0c16be553781e22ebdf
Alexandre Hirata <alexthirata@gmail.com>
2020-06-10 12:36:35 -0300

    Add import-export to Part in Admin panel
    Signed-off-by: Alexandre Hirata <alexthirata@gmail.com>

--- app/qualiCar_API/admin.py
+++ app/qualiCar_API/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from qualiCar_API import models
+from qualiCar_API.models import Part
 
 admin.site.register (models.Date)
 
@@ -27,8 +31,11 @@ class UserAdmin (BaseUserAdmin):
         }),
     )
 
+class PartAdmin (ImportExportModelAdmin):
+    pass
+
 # These lines adds models to admin page
 admin.site.register (models.UserProfile, UserAdmin)
-admin.site.register (models.Part)
+admin.site.register (models.Part, PartAdmin)
 admin.site.register (models.Vehicle)
 admin.site.register (models.Incident)
